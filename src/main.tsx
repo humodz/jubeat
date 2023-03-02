c060d0dddd570aed1227010c15b96023a1029ff4
Hugo Dzin <humodz065@gmail.com>
2023-03-02 10:09:41 -0300

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'simpledotcss';
 import { App } from './App';
 import './index.css';
-import 'simpledotcss';
 import './simpledotcss-tweaks.css';
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
