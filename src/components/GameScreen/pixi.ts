dba705bf1ca9306ac190b2a3687dd4e8f6ab9ece
Hugo Dzin <humodz065@gmail.com>
2023-03-03 14:11:42 -0300

    refactor

--- src/components/GameScreen/pixi.ts
+++ src/components/GameScreen/pixi.ts
@@ -51,75 +51,132 @@ function initTouch(canvas: HTMLCanvasElement) {
 export async function initPixi(pixi: PIXI.Application<HTMLCanvasElement>) {
   const touchList = initTouch(pixi.view);
 
-  function createButtonOutline(x: number, y: number) {
-    const gfx = new PIXI.Graphics();
-    gfx.lineStyle(5, 0x3399ff, 1, 0);
-    gfx.beginFill(0, 0);
-    gfx.drawRect(0, 0, 100, 100);
-    gfx.endFill();
+  const buttonPad = new ButtonPad({
+    buttonSize: 100,
+    gridCols: 4,
+    gridRows: 4,
+  });
+  const touchMarkers = new TouchMarkers();
 
-    gfx.x = x * 100;
-    gfx.y = y * 100;
-
-    pixi.stage.addChild(gfx);
-    return gfx;
-  }
-
-  function createTouchMarker() {
-    const gfx = new PIXI.Graphics();
-    gfx.beginFill(0xff0000, 0.5);
-    gfx.drawCircle(0, 0, 20);
-    gfx.endFill();
-    gfx.visible = true;
-    pixi.stage.addChild(gfx);
-    return gfx;
-  }
-
-  const touchMarkers: PIXI.Graphics[] = [];
-
-  const buttons = range(4)
-    .map((row) => range(4).map((col) => createButtonOutline(col, row)))
-    .flat();
+  pixi.stage.addChild(buttonPad.node);
+  pixi.stage.addChild(touchMarkers.node);
 
   pixi.ticker.add(() => {
-    if (touchMarkers.length < touchList.length) {
-      const amount = touchList.length - touchMarkers.length;
-      const newMarkers = repeat(amount, () => createTouchMarker());
-      touchMarkers.push(...newMarkers);
+    buttonPad.tick(touchList);
+    touchMarkers.tick(touchList);
+  });
+}
+
+class TouchMarkers {
+  node = new PIXI.Container();
+  markers: PIXI.Graphics[] = [];
+
+  tick(touchPoints: TouchPoint[]) {
+    if (this.markers.length < touchPoints.length) {
+      const amount = touchPoints.length - this.markers.length;
+      const newMarkers = repeat(amount, () => this.createTouchMarker());
+      this.markers.push(...newMarkers);
+      this.node.addChild(...newMarkers);
     }
 
-    touchMarkers.forEach((marker, i) => {
-      if (i >= touchList.length) {
+    this.markers.forEach((marker, i) => {
+      if (i >= touchPoints.length) {
         marker.visible = false;
       } else {
         marker.visible = true;
-        const touch = touchList[i];
+        const touch = touchPoints[i];
         marker.x = touch.x;
         marker.y = touch.y;
       }
     });
-
-    const isTouched = repeat(buttons.length, () => false);
-
-    for (const touch of touchList) {
-      const r = 10;
-      check(isTouched, touch.x - r, touch.y - r);
-      check(isTouched, touch.x - r, touch.y + r);
-      check(isTouched, touch.x + r, touch.y - r);
-      check(isTouched, touch.x + r, touch.y + r);
-    }
-
-    buttons.forEach((it, i) => (it.visible = isTouched[i]));
-  });
-}
-
-function check(isTouched: boolean[], x: number, y: number) {
-  const quadrantX = Math.floor(x / 100);
-  const quadrantY = Math.floor(y / 100);
-
-  if (quadrantX < 0 || quadrantX >= 4 || quadrantY < 0 || quadrantY >= 4) {
-    return;
   }
 
-  isTouched[quadrantX + quadrantY * 4] = true;
+  createTouchMarker() {
+    return new PIXI.Graphics()
+      .beginFill(0xff0000, 0.5)
+      .drawCircle(0, 0, 20)
+      .endFill();
+  }
+}
+
+interface ButtonPadProps {
+  buttonSize: number;
+  gridRows: number;
+  gridCols: number;
+}
+class ButtonPad {
+  node = new PIXI.Container();
+  buttons: GameButton[];
+
+  constructor(public props: ButtonPadProps) {
+    this.buttons = range(props.gridCols * props.gridRows).map(
+      (i) =>
+        new GameButton({
+          size: props.buttonSize,
+          x: props.buttonSize * (i % props.gridCols),
+          y: props.buttonSize * Math.floor(i / props.gridCols),
+        }),
+    );
+
+    this.node.addChild(...this.buttons.map((it) => it.node));
+  }
+
+  tick(touchPoints: TouchPoint[]) {
+    const isTouched = repeat(this.buttons.length, () => false);
+
+    for (const touch of touchPoints) {
+      const r = 10;
+      this.checkIsTouched(isTouched, touch.x - r, touch.y - r);
+      this.checkIsTouched(isTouched, touch.x - r, touch.y + r);
+      this.checkIsTouched(isTouched, touch.x + r, touch.y - r);
+      this.checkIsTouched(isTouched, touch.x + r, touch.y + r);
+    }
+
+    this.buttons.forEach((it, i) => {
+      it.showOutline(isTouched[i]);
+    });
+  }
+
+  checkIsTouched(isTouched: boolean[], x: number, y: number) {
+    const quadrantX = Math.floor(x / this.props.buttonSize);
+    const quadrantY = Math.floor(y / this.props.buttonSize);
+
+    if (
+      quadrantX < 0 ||
+      quadrantX >= this.props.gridCols ||
+      quadrantY < 0 ||
+      quadrantY >= this.props.gridRows
+    ) {
+      return;
+    }
+
+    isTouched[quadrantX + quadrantY * this.props.gridCols] = true;
+  }
+}
+
+interface GameButtonProps {
+  size: number;
+  x: number;
+  y: number;
+}
+class GameButton {
+  node = new PIXI.Container();
+  outline = new PIXI.Graphics();
+
+  constructor(public props: GameButtonProps) {
+    this.node.x = props.x;
+    this.node.y = props.y;
+
+    this.outline
+      .lineStyle(5, 0x3399ff, 1, 0)
+      .beginFill(0, 0)
+      .drawRect(0, 0, props.size, props.size)
+      .endFill();
+
+    this.node.addChild(this.outline);
+  }
+
+  showOutline(visible: boolean) {
+    this.outline.visible = visible;
+  }
 }
